5742527dcc92a2028de8926daad554dc0a036485
TareqMonwer <tareqmonwer137@gmail.com>
2021-04-10 21:38:59 +0600

    add seat filtering and booking views and logics.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
+import datetime as dt
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Ticket, CarSeat, Car
 
 
 def home(request):
@@ -7,11 +9,56 @@ def home(request):
 
 
 def seat_planner(request):
-    return render(request, 'core/seats.html')
+    car = Car.objects.get(id=1)
+    seats = CarSeat.objects.filter(car=car)
+    departure_city = request.GET.get('from')
+    destination = request.GET.get('to')
+
+    if request.method == 'GET':
+        departure_city = request.GET.get('from')
+        destination = request.GET.get('to')
+        departure_date = request.GET.get('date')
+        departure_time = request.GET.get('time')
+        departure_time = dt.datetime(
+            *list(map(int, departure_date.split('-'))),
+            *list(map(int, departure_time.split('.')))
+        )
+        request.session['booking_time'] = departure_time.strftime(
+            '%Y-%m-%d %H.%M'
+        )
+        booked_tickets = Ticket.objects.filter(
+            departure_at=departure_time,
+            departure_city=departure_city
+        )
+        booked_seats = [ticket.seat for ticket in booked_tickets]
+        ctx = {
+            'seats': seats,
+            'booked_seats': booked_seats,
+            'departure_city': departure_city,
+            'destination': destination
+        }
+        return render(request, 'core/seats.html', ctx)
+
+    elif request.method == 'POST':
+        seats = set(request.POST.getlist('seats'))
+        from_city = request.POST.get('from_city').lower()
+        to_city = request.POST.get('to_city').lower()
+
+        for seat in seats:
+            seat = CarSeat.objects.get(car=car, number=seat)
+            dept_time = dt.datetime.strptime(
+                request.session['booking_time'],
+                '%Y-%m-%d %H.%M'
+            )
+            Ticket.objects.create(
+                seat=seat,
+                departure_at=dept_time,
+                departure_city=from_city
+            )
+            del request.session['booking_time']
+        msg = ', '.join(seats) + ' You booked these seats.'
+        return HttpResponse(msg)
 
 
 def pay(request):
-    if request.method == 'POST':
-        seats = request.POST.getlist('seats')
-        msg = ', '.join(seats) + ' You booked these seats.'
-        return HttpResponse(msg)
+    return Response('PAID..')
